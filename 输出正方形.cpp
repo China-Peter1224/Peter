a047ca063ec10305f736c6e335446fec17a22470
Peter <ic1101w@126.com>
2026-04-21 18:55:40 +0800

    Add files via upload

---
+++ 输出正方形.cpp
@@ -0,0 +1,33 @@
+#include <iostream> 
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++)
+	{
+		if(i==1||i==n)
+		{
+			for(int j=1;j<=n;j++)
+			{
+				if(j==1||j==n)
+					cout<<"+";
+				else
+					cout<<"-";
+			}
+			cout<<endl;
+		}
+		else
+		{
+			for(int k=1;k<=n;k++)
+			{
+				if(k==1||k==n)
+					cout<<"|";
+				else
+					cout<<"*";
+			}
+			cout<<endl;	
+		}
+	}
+	return 0;
+}
